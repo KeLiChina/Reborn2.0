e817d82c7c8b04fade0598ee71cbbff5ae2db3a8
LywockeeZ <kofln@vip.qq.com>
2018-12-09 03:36:07 +0800

    bone

---
+++ Assets/Scripts/IntoTrap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntoTrap : MonoBehaviour {
+    public Animator left;
+    public Animator right;
+
+    void OnTriggerEnter2D(Collider2D obj)
+    {
+
+
+        left.SetTrigger("Start");
+        right.SetTrigger("Start");
+
+    }
+}
